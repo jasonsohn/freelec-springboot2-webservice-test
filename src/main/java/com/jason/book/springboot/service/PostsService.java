30c311da496a835aa70ecca2363184df1b72dd6e
jasonsohn <jason.sohn28@gmail.com>
2020-05-14 17:42:07 +0900

    feat : Posts 도메인개념 관련 클래스 생성

---
+++ src/main/java/com/jason/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.jason.book.springboot.service;
+
+import com.jason.book.springboot.domain.posts.PostsRepository;
+import com.jason.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/jason/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,4 @@
+package com.jason.book.springboot.web.dto;
+
+public class PostsSaveRequestDto {
+}
