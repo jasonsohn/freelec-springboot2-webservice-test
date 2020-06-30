ee6cb844eb43bf75f98f1dc646773c8b4c64d803
jasonsohn <jason.sohn28@gmail.com>
2020-06-30 16:47:03 +0900

    feat : Post 수정 테스트코드 추가

--- src/test/java/com/jason/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jason/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jason.book.springboot.web;
 import com.jason.book.springboot.domain.posts.Posts;
 import com.jason.book.springboot.domain.posts.PostsRepository;
 import com.jason.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jason.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -58,5 +60,31 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception {
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+
     }
 }
